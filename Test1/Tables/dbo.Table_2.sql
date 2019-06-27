ec571336f2d1e5261ca8d49a7716dc52dc23ffff
acapop@sbb.rs <>
2019-06-27 16:55:00 +0200

    ApexSQL Model commit

--- Test1/Tables/dbo.Table_2.sql
+++ Test1/Tables/dbo.Table_2.sql
@@ -3,19 +3,12 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_2] (
-		[New_column_1]      [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL,
-		[New_column_2]      [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL,
-		[New_column_3]      [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL,
-		[Table_1_Id_FK]     [int] NULL
+		[New_column_1]     [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL,
+		[New_column_2]     [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL,
+		[New_column_3]     [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL,
+		[New_column_4]     [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL,
+		[New_column_5]     [nchar](10) COLLATE Latin1_General_CI_AS_KS_WS NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Table_2]
-	WITH CHECK
-	ADD CONSTRAINT [Table_2_Table_1_FK]
-	FOREIGN KEY ([Table_1_Id_FK]) REFERENCES [dbo].[Table_1] ([Table_1_Id])
-ALTER TABLE [dbo].[Table_2]
-	CHECK CONSTRAINT [Table_2_Table_1_FK]
-
 GO
 ALTER TABLE [dbo].[Table_2] SET (LOCK_ESCALATION = TABLE)
 GO
